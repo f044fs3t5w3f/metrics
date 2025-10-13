e9d447f319debbab4b375e3e66ac629090e00e1d
Anton Kuleshov <secret@example.com>
2025-10-13 21:40:13 +0300

    fix bug PollInterval

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -13,6 +13,6 @@ var (
 func parseFlags() {
 	flag.StringVar(&flagEndpointAddr, "a", "localhost:8080", "endpoint address and port")
 	flag.Int64Var(&flagReportInterval, "r", 10, "report interval")
-	flag.Int64Var(&flagReportInterval, "p", 2, "poll interval")
+	flag.Int64Var(&flagPollInterval, "p", 2, "poll interval")
 	flag.Parse()
 }
